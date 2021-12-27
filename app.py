30a5cfd88c071c06fd58be1155b074b502bb7aff
datascientist1234 <96609488+datascientist1234@users.noreply.github.com>
2021-12-27 19:21:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ class VideoTransformer(VideoTransformerBase):
             final_image = img_to_array(final_image)
             prediction = my_model.predict(final_image)[0]
             label=class_labels[prediction.argmax()]
-            cv2.putText(img,label, (50,60), cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,255),2)    
+            cv2.putText(img,label, (50,60), cv2.FONT_HERSHEY_SIMPLEX,2, (0,0,255),2)    
         return img
 webrtc_streamer(
         key="object-detection",

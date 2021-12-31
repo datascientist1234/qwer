efff1e7e3c6354e696107434ae921f213fd16b2c
datascientist1234 <96609488+datascientist1234@users.noreply.github.com>
2021-12-31 19:24:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May  1 17:57:30 2021
+'''This code is for running the model on streamlit as a web application.
+please click on the link given in readme file to watch it work'''
 
-@author: Apoorva
-"""
-import streamlit as st
-st.title('Face emotion recognition')
 import cv2
 import numpy as np
-import streamlit as st
-from streamlit_webrtc import (
-    ClientSettings,
-    VideoTransformerBase,
-    WebRtcMode,
-    webrtc_streamer,
-)
 from keras.models import load_model
+import streamlit as st
+from streamlit_webrtc import ClientSettings,VideoTransformerBase,WebRtcMode,webrtc_streamer
 
 
+st.title('Real Time Face emotion recognition')
 
-my_model=load_model('model.h5')
+model=load_model('model.h5')
+
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
     rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-    media_stream_constraints={"video": True, "audio": True},
+    media_stream_constraints={"video": True, "audio": False},
 )
-
-
-
 
 class VideoTransformer(VideoTransformerBase):
     def transform(self, frame):
@@ -39,33 +28,31 @@ class VideoTransformer(VideoTransformerBase):
 
         
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        face_roi = face_detect.detectMultiScale(img_gray, 1.3,1)
-        if face_roi is ():
+        faces = face_detect.detectMultiScale(img_gray, 1.3,1)
+        if faces is ():
             return img
 
-        for(x,y,w,h) in face_roi:
+        for(x,y,w,h) in faces:
             x = x - 5
             w = w + 10
             y = y + 7
             h = h + 2
             
-            cv2.rectangle(img, (x,y+10),(x+w,y+h),(0,255,255), 3)
-            img_color_crop = img[y:y+h,x:x+w]
-            #img_color_crop = img[y:y+h,x:x+w]                        
-            final_image = cv2.resize(img_color_crop, (48,48))
+            cv2.rectangle(img, (x,y+10),(x+w,y+h),(0,0,255), 3)
+            img_crop = img[y:y+h,x:x+w]
+            img_crop = img[y:y+h,x:x+w]                        
+            final_image = cv2.resize(img_crop, (48,48))
             final_image = np.expand_dims(final_image, axis = 0)
             final_image = final_image/255.0
-            prediction = my_model.predict(final_image)
+            prediction = model.predict(final_image)
             label=class_labels[prediction.argmax()]
-            cv2.putText(img,label,(x,y), cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,255),2)    
+            cv2.putText(img,label,(x,y), cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)      
         return img
+
 webrtc_streamer(
-        key="object-detection",
+        key="emotion-detection",
         mode=WebRtcMode.SENDRECV,
         client_settings=WEBRTC_CLIENT_SETTINGS,
         video_transformer_factory=VideoTransformer,
         async_transform=True,
     )
-
-
-

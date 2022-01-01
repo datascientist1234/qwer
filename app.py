0917430d9ccb28c33496dc423087c6e25126a7f5
datascientist1234 <96609488+datascientist1234@users.noreply.github.com>
2022-01-01 12:16:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ model=load_model('model.h5')
 
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
     rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-    media_stream_constraints={"video": True, "audio": False},
+    media_stream_constraints={"video": True, "audio": True},
 )
 
 class VideoTransformer(VideoTransformerBase):

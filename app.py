debc3cd987314fdbb1e231f838ed61de52ea3895
datascientist1234 <96609488+datascientist1234@users.noreply.github.com>
2022-01-02 11:43:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,13 @@
 '''
-This python script contains code to create a stream lit app. This app detects facial emotions end to end.
-
-To run this file "locally", type 
-                    
-                    streamlit run app.py --server.maxUploadSize=50
-                    
-Files required to run this app:
-1. app.py
-2. haarcascade_frontalface_default.xml
-3. model.h5
-4. demo_images    (optional) if you want to see demo images
+This is a script to run the project as a web application on streamlit.
+simply click on the link provided in the Readme file to watch a demonstration.
 '''
 
-# importing relevant libraries
-#import os
-#import av
+#import libraries
 import cv2
-#import time
-#import tempfile
 import numpy as np
-#from PIL import Image
 import streamlit as st
-#from time import sleep
-#from aiortc.contrib.media import MediaPlayer
 from tensorflow.keras.models import load_model
-#from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import img_to_array
 from streamlit_webrtc import (
     ClientSettings,
@@ -33,52 +16,48 @@ from streamlit_webrtc import (
     webrtc_streamer,
 )
 
-
+# function to configure webcam for streamlit and turn off mic
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
     rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
     media_stream_constraints={"video": True, "audio": False},
 )
 
-# importing the necessary files
+# importing the model
 faceCascade = cv2.CascadeClassifier(r'haarcascade_frontalface_default.xml')
 model =load_model(r'model.h5')
 
+#emotions
 emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
 
 
-# a class that captures real time webcam feed
+# emotion prediction 
 class VideoTransformer(VideoTransformerBase):
     def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(gray)
         if faces is ():
-            return img
+            print("no faces detected")
 
         for (x,y,w,h) in faces:
             cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,255),2)
-            roi_gray = gray[y:y+h,x:x+w]
-            roi_gray = cv2.resize(roi_gray,(48,48))
+            img_crop = gray[y:y+h,x:x+w]
+            img_crop = cv2.resize(img_crop,(48,48))
 
-            roi = roi_gray.astype('float')/255.0
-            roi = img_to_array(roi)
-            roi = np.expand_dims(roi,axis=0)
+            final_img = img_crop.astype('float')/255.0
+            final_img = img_to_array(final_img)
+            final_img = np.expand_dims(final_img,axis=0)
 
-            prediction = model.predict(roi)[0]
+            prediction = model.predict(final_img)[0]
             label=emotion_labels[prediction.argmax()]
-            label_position = (x,y-10)
-            cv2.putText(img,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)    
+            cv2.putText(img,label,(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)    
         return img
 
-
-
-# main function
 webrtc_streamer(
-        key="object-detection",
+        key="emotion-detection",
         mode=WebRtcMode.SENDRECV,
         client_settings=WEBRTC_CLIENT_SETTINGS,
         video_transformer_factory=VideoTransformer,
         async_transform=True,
     )
         
-
